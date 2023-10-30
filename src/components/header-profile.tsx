ab5917afd09e6290af2691e278cc96f946e73f86
Damir Modyarov <otomir@yandex.ru>
2023-10-30 15:43:27 +0300

    Improve header appearance

--- src/components/header-profile.tsx
+++ src/components/header-profile.tsx
@@ -5,12 +5,12 @@ import {LogInIcon, LogOutIcon} from "lucide-react";
 export default async function HeaderProfile() {
     const profile = await getProfile()
     if (!profile) return (
-        <Link href="/auth" className="flex gap-1 items-center">
+        <Link href="/auth" className="flex gap-2 items-center">
             <LogInIcon size={16} /> Login
         </Link>
     )
     return (
-        <div className="flex gap-1 items-center">
+        <div className="flex gap-2 items-center">
             <Link href={`/u/${profile.username}`}>@{profile.username}</Link>
             <Link href="/auth/logout"><LogOutIcon size={16} /></Link>
         </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Header() {
 
     return (
-        <header className="sticky top-0 inset-x-0 bg-blue-50 py-6 text-blue-950 flex justify-between mb-4 px-12 sm:px-16">
+        <header className="sticky top-0 inset-x-0 bg-white border-b border-blue-50 py-4 text-blue-950 flex justify-between mb-4 px-12 sm:px-16">
             <Link href="/" className="font-bold text-lg">Cerulean</Link>
             <Suspense fallback={<div className="h-4 w-24 bg-blue-300 animate-pulse" />}>
                 <HeaderProfile />
