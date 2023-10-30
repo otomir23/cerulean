635c97a15b793ce750c0bde9b4a58dc8a1badf5c
Damir Modyarov <otomir@yandex.ru>
2023-10-30 22:11:07 +0300

    Improve button appearance

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,12 +4,12 @@ import {VariantProps} from "cva";
 import {LucideIcon} from "lucide-react";
 
 const buttonStyles = cva({
-    base: `flex flex-wrap items-center justify-center gap-2 rounded px-4 py-2 select-none
-    font-semibold transition-colors focus:outline-none focus:ring disabled:animate-pulse`,
+    base: `flex flex-wrap items-center justify-center gap-2 rounded-md px-4 py-2 select-none border
+    font-semibold transition-colors focus:outline-none focus:ring disabled:animate-pulse disabled:cursor-not-allowed`,
     variants: {
         intent: {
-            primary: 'bg-blue-600 text-blue-50 ring-blue-300 hover:bg-blue-600 active:bg-blue-500 disabled:text-blue-400',
-            secondary: 'bg-neutral-50 text-neutral-900 ring-neutral-300 hover:bg-neutral-100 active:bg-neutral-50 disabled:text-neutral-400',
+            primary: 'bg-blue-600 text-blue-50 ring-blue-300 hover:bg-blue-600 active:bg-blue-500 disabled:text-blue-400 border-blue-800',
+            secondary: 'bg-neutral-50 text-neutral-900 ring-neutral-200 hover:bg-neutral-100 active:bg-neutral-50 disabled:text-neutral-400 border-neutral-300',
         },
     },
     defaultVariants: {
